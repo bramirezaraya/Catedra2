34444499f562323b6760fa224eb511cd0c536e3c
Bryan Ramirez <bryan.ramirez.a@mail.pucv.cl>
2021-10-12 20:52:28 -0300

    subiendo archivos

---
+++ js/inicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function mostrarFormulario() {
+    var _a;
+    (_a = document.querySelector('#container .formulario')) === null || _a === void 0 ? void 0 : _a.classList.add('formulario-activo');
+}
+function AgregarAntecedentes() {
+    var _a;
+    (_a = document.querySelector('#container .motivoHospital')) === null || _a === void 0 ? void 0 : _a.classList.add('motivoHospital-activo');
+}
---
+++ ts/inicio.ts
@@ -0,0 +1,12 @@
+
+
+
+
+function mostrarFormulario(){
+    document.querySelector('#container .formulario')?.classList.add('formulario-activo');
+}
+
+
+function AgregarAntecedentes(){
+    document.querySelector('#container .motivoHospital')?.classList.add('motivoHospital-activo');
+}
